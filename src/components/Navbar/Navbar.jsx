b67633fbc331d1c80c58e6fe83ad15656ba94c6f
tin <minhtien0601200@gmail.com>
2024-05-11 22:50:39 +0700

    modified tailwind config and Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,6 +12,20 @@ const Navbar = () => {
                     <img src={Logo} alt="" />
                 </a>
             </div>
+            {/* search bar and order button */}
+            <div>
+                <div className="group">
+                    <input 
+                        type="text" placeholder="search" 
+                        className="w-[200px] sm:w-[200px]
+                        group-hover:w-[300px] transition-all
+                        duration-300 rounded-full border
+                        border-gray-300 px-2 py-1 
+                        focus:outline-none focus:border-1
+                        focus:border-primary"
+                    />
+                </div>
+            </div>
         </div>
     </div>
     {/* lower Navbar */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      container:{
+        center : true,
+        padding:{
+          DEFAULT: "1rem",
+          sm: "3rem",
+        }
+      }
+    },
   },
   plugins: [],
 }
